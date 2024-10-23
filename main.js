9660f71378b98401af421305fb843507c6b538f2
briane <brianecastada11@gmail.com>
2024-10-24 05:18:49 +0800

    Fix line endings

--- main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './app.vue';
-
-const app = createApp(App);
-const pinia = createPinia();
-
-app.use(pinia);
-app.mount('#app');
